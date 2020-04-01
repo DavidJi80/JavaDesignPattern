9cb34d58e7d30696d703459081c0e766636f3d71
David Ji <297933227@qq.com>
2020-04-01 16:07:25 +0800

    适配器模式（Adapter Pattern）

---
+++ src/main/java/com/github/davidji80/java_designpattern/designpattern/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package com.github.davidji80.java_designpattern.designpattern.adapter;
+
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+    public void playMp4(String fileName);
+}
---
+++ src/main/java/com/github/davidji80/java_designpattern/designpattern/adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.github.davidji80.java_designpattern.designpattern.adapter;
+
+public interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}
